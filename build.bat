8ed665d2fb7787f2746860b4e7734673651ddcbc
zhaozg <zhaozg@gmail.com>
2015-04-12 19:45:11 +0800

    update 1.0.2

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 E:\portableDev\portable.cmd
 perl Configure -DOPENSSL_CCSTC VC-WIN32 --prefix=..\1.0.2
 ms\do_nasm
+nmake -f ms\nt.mak clean
 nmake -f ms\nt.mak
 nmake -f ms\nt.mak install
-nmake -f ms\nt.mak clean
