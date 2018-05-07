d9683c707d1d9d88b5c6a764a07c02fbebe5d962
George Zhao <zhaozg@gmail.com>
2018-05-07 20:13:20 +0800

    update

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-perl Configure VC-WIN32 no-hw no-shared no-zlib --prefix=..\master --openssldir=..\master
+perl Configure VC-WIN32 no-hw no-shared no-zlib --prefix=e:\work\product\openssl\master --openssldir=e:\work\product\openssl\master
 nmake
 nmake test
 nmake install
